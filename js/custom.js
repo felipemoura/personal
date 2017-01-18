f5981f7bb25524f9ee4076355056b5d8281433d3
Felipe Moura <mourafelp@gmail.com>
2017-01-18 18:12:28 -0200

    v1.0.0
    First Official change, created portfolio page with some css and js.
    
    Hope to improve the design and quality soon.

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	//remove the active class from all items, if there is any
+	$('.nav>li').removeClass('active');
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 40
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
